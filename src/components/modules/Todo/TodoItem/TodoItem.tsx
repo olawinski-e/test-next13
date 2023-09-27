d2dfd815d7f4c17d0cadddd249a25ffdfa5c3abc
Estelle Olawinski <estelle.olawinski@me.com>
2023-09-27 15:45:16 +0200

    feat: add link see +

--- src/components/modules/Todo/TodoItem/TodoItem.tsx
+++ src/components/modules/Todo/TodoItem/TodoItem.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { updateTodoItem } from '../../../../services/todo.service'
+import { updateTodoItem, deleteTodoItem } from '../../../../services/todo.service'
 import { ITodo } from '../../../../types/types'
 import Button from '../../../common/Button'
 import Card from '../../../common/Card'
@@ -12,12 +12,17 @@ import styles from './TodoItem.module.scss'
 
 export default function TodoItem({ id, title, completed }: ITodo) {
   async function updateTodo(e) {
+    e.stopPropagation()
     await updateTodoItem(id, e.target.checked)
+  }
+
+  async function deleteTodo() {
+    await deleteTodoItem(id)
   }
 
   return (
     <Card todoStatus={completed}>
-      <Link href={`/todo/${id}`} className={styles.linkCard}>
+      <div className={styles.linkCard}>
         <div className={styles.todoItemImg}>
           <Image
             src={`https://api.slingacademy.com/public/sample-photos/${id}.jpeg`}
@@ -27,12 +32,15 @@ export default function TodoItem({ id, title, completed }: ITodo) {
         </div>
         <div className={styles.todoItem}>
           <div className={styles.todoItemButtons}>
-            <Input id={id} type="checkbox" completed={completed} onChange={updateTodo} />
-            <Button severity="danger" size="small" label="X" />
+            <Input id={id} type="checkbox" completed={completed} onChange={(e) => updateTodo(e)} />
+            <Button severity="danger" size="small" label="X" onClick={deleteTodo} />
           </div>
-          <b>{title}</b>
+          <h3>{title}</h3>
+          <Link href={`/todo/${id}`}>
+            <h4 className={styles.seeMore}>Voir +</h4>
+          </Link>
         </div>
-      </Link>
+      </div>
     </Card>
   )
 }
